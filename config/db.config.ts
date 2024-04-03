c3e9af02a86b19d86d7a676b28b98e7b2675c985
sajad-ahmadnzhad <143605677+sajad-ahmadnzhad@users.noreply.github.com>
2024-04-03 15:55:25 +0430

    feat: create connection

--- config/db.config.ts
+++ config/db.config.ts
@@ -1,5 +1,20 @@
-import {Sequelize} from '@sequelize/core'
-
+import { Sequelize } from "@sequelize/core";
+const { USER, PASS, HOST, DATABASE } = process.env;
 const connection = new Sequelize({
-    
-})
+  host: HOST,
+  username: USER,
+  password: PASS,
+  database: DATABASE,
+  dialect: "mysql",
+});
+
+(async () => {
+    try {
+        await connection.authenticate()
+        console.log('Connect to db successfully')
+    } catch (error) {
+        console.log('Connect to db error => ' , error)
+    }
+})()
+
+export default connection
